afb65b448f9cd1c9c97b13abd2cbe7b54a49ab21
potatooBot <kumarkeshav825@gmail.com>
2023-06-23 17:39:22 +0530

    Contact updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -87,7 +87,7 @@ else{
                           name="name"
                               type="text"
                               required
-                              placeholder='Gurdeep Singh'
+                              placeholder='Eg-Deep Sharma'
                               className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
                           value={userData.name}
                           onChange={postUserData}
