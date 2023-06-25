74354de63dcdbadc89a8bc217cdc6509b463baf7
potatooBot <kumarkeshav825@gmail.com>
2023-06-25 22:23:19 +0530

    Partners updated

--- src/components/Partnersection.jsx
+++ src/components/Partnersection.jsx
@@ -21,11 +21,11 @@ const responsive = {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 4
+      items: 3
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 4
+      items: 3
     }
   };
 const sliderImageUrl = [
