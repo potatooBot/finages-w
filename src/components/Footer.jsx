606951ab3df9a058ad6d9f39e9df13359602952d
potatooBot <kumarkeshav825@gmail.com>
2023-06-25 22:32:24 +0530

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,9 +11,18 @@ const Footer = () => (
           alt="Finages"
           className="w-[266px] h-[72.14px] object-contain"
         />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px] text-black`}>
+        {/* <p className={`${styles.paragraph} mt-4 max-w-[312px] text-black`}>
           A new way to make the payments easy, reliable and secure.
-        </p>
+        </p> */}
+        <h2 className={`font-poppins font-normal text-[20px] leading-[30.8px] mt-4 max-w-[312px] text-black`}>
+    19,Netaji Subhas Rd,Fairley Place,B.B.D Bagh,Kolkata,West Bengal,700001
+      </h2>
+        <h2 className={`${styles.paragraph} mt-4 max-w-[312px] text-black`}>
+        Contact : 9830116792
+      </h2>
+      <h2 className={`${styles.paragraph} mt-4 max-w-[312px] text-black`}>
+       finagesdistribution@gmail.com
+      </h2>
       </div>
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10 text-black">
@@ -42,9 +51,10 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
+    <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-500">
         Copyright Ⓒ 2023 Finages. All Rights Reserved.
       </p>
+     
 
       <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
